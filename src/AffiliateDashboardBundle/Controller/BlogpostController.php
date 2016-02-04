092d0bb889642ba921c2756a8637c2121768c86d
Matthias Kleine <matthias@klein0r.de>
2016-02-04 22:00:16 +0100

    Added blogposts

---
+++ src/AffiliateDashboardBundle/Controller/BlogpostController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AffiliateDashboardBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AffiliateDashboardBundle\Entity\Blogpost;
+use AffiliateDashboardBundle\Form\BlogpostType;
+
+/**
+ * Blogpost controller.
+ *
+ * @Route("/blogpost")
+ */
+class BlogpostController extends Controller
+{
+    /**
+     * Lists all Blogpost entities.
+     *
+     * @Route("/", name="blogpost_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blogposts = $em->getRepository('AffiliateDashboardBundle:Blogpost')->findAll();
+
+        return $this->render('AffiliateDashboardBundle:Blogpost:index.html.twig', array(
+            'blogposts' => $blogposts,
+        ));
+    }
+
+    /**
+     * Creates a new Blogpost entity.
+     *
+     * @Route("/new", name="blogpost_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $blogpost = new Blogpost();
+        $form = $this->createForm('AffiliateDashboardBundle\Form\BlogpostType', $blogpost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blogpost);
+            $em->flush();
+
+            return $this->redirectToRoute('blogpost_show', array('id' => $blogpost->getId()));
+        }
+
+        return $this->render('AffiliateDashboardBundle:Blogpost:new.html.twig', array(
+            'blogpost' => $blogpost,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Blogpost entity.
+     *
+     * @Route("/{id}", name="blogpost_show")
+     * @Method("GET")
+     */
+    public function showAction(Blogpost $blogpost)
+    {
+        $deleteForm = $this->createDeleteForm($blogpost);
+
+        return $this->render('AffiliateDashboardBundle:Blogpost:show.html.twig', array(
+            'blogpost' => $blogpost,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Blogpost entity.
+     *
+     * @Route("/{id}/edit", name="blogpost_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Blogpost $blogpost)
+    {
+        $deleteForm = $this->createDeleteForm($blogpost);
+        $editForm = $this->createForm('AffiliateDashboardBundle\Form\BlogpostType', $blogpost);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blogpost);
+            $em->flush();
+
+            return $this->redirectToRoute('blogpost_edit', array('id' => $blogpost->getId()));
+        }
+
+        return $this->render('AffiliateDashboardBundle:Blogpost:edit.html.twig', array(
+            'blogpost' => $blogpost,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Blogpost entity.
+     *
+     * @Route("/{id}", name="blogpost_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Blogpost $blogpost)
+    {
+        $form = $this->createDeleteForm($blogpost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($blogpost);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('blogpost_index');
+    }
+
+    /**
+     * Creates a form to delete a Blogpost entity.
+     *
+     * @param Blogpost $blogpost The Blogpost entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Blogpost $blogpost)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('blogpost_delete', array('id' => $blogpost->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AffiliateDashboardBundle/Entity/Blogpost.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace AffiliateDashboardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Blogpost
+ *
+ * @ORM\Table(name="blogpost")
+ * @ORM\Entity(repositoryClass="AffiliateDashboardBundle\Repository\BlogpostRepository")
+ */
+class Blogpost
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="published_at", type="datetime")
+     */
+    private $publishedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="affiliate_tag", type="string", length=255)
+     */
+    private $affiliateTag;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Blogpost
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set publishedAt
+     *
+     * @param \DateTime $publishedAt
+     *
+     * @return Blogpost
+     */
+    public function setPublishedAt($publishedAt)
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get publishedAt
+     *
+     * @return \DateTime
+     */
+    public function getPublishedAt()
+    {
+        return $this->publishedAt;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Blogpost
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Get affiliateTag
+     *
+     * @return string
+     */
+    public function getAffiliateTag()
+    {
+        return $this->affiliateTag;
+    }
+
+    /**
+     * Set affiliateTag
+     *
+     * @param string $affiliateTag
+     */
+    public function setAffiliateTag($affiliateTag)
+    {
+        $this->affiliateTag = $affiliateTag;
+    }
+}
+
