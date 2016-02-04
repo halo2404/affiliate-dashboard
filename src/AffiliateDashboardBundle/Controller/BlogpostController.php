b3ff108d47bb9e8faf96ff493b45209aee0f7856
Matthias Kleine <matthias@klein0r.de>
2016-02-04 22:09:15 +0100

    Bootstrap stuff

--- src/AffiliateDashboardBundle/Controller/BlogpostController.php
+++ src/AffiliateDashboardBundle/Controller/BlogpostController.php
@@ -12,7 +12,7 @@ use AffiliateDashboardBundle\Form\BlogpostType;
 /**
  * Blogpost controller.
  *
- * @Route("/blogpost")
+ * @Route("/{_locale}/blogpost")
  */
 class BlogpostController extends Controller
 {
