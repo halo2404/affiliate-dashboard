88664dcc561ed30c6451baa643bebd64e92acae5
Matthias Kleine <matthias@klein0r.de>
2016-02-08 22:46:36 +0100

    Tag Statistics

--- src/AffiliateDashboardBundle/Controller/ChartController.php
+++ src/AffiliateDashboardBundle/Controller/ChartController.php
@@ -2,6 +2,7 @@
 
 namespace AffiliateDashboardBundle\Controller;
 
+use AffiliateDashboardBundle\Entity\Tag;
 use AffiliateDashboardBundle\Service\Xmlfile;
 use Ob\HighchartsBundle\Highcharts\Highchart;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -36,7 +37,7 @@ class ChartController extends Controller
 
         // Chart
         $series = array(
-            array('name' => 'Earnings', 'data' => array_values($sales))
+            array('name' => $translator->trans('Earnings'), 'data' => array_values($sales))
         );
 
         $ob = new Highchart();
@@ -44,7 +45,7 @@ class ChartController extends Controller
         $ob->chart->type('line');
         $ob->chart->renderTo('chart');
 
-        $ob->title->text($translator->trans('Sales per Month'));
+        $ob->title->text($translator->trans('Sales per month'));
 
         $ob->xAxis->categories(array_keys($sales));
         $ob->xAxis->title(array('text' => $translator->trans('Month')));
@@ -58,5 +59,97 @@ class ChartController extends Controller
         );
     }
 
+    /**
+     * Sales per tag
+     *
+     * @Route("/tag/{id}", name="chart_tag")
+     * @Method("GET")
+     */
+    public function tagAction(Tag $tag)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $translator = $this->get('translator');
 
+        $sales = $em->getRepository('AffiliateDashboardBundle:Sale')->getSalesPerDay($tag);
+
+        // Chart
+        $series = array(
+            array('name' => $translator->trans('Earnings'), 'data' => array_values($sales))
+        );
+
+        $ob = new Highchart();
+
+        $ob->chart->type('line');
+        $ob->chart->renderTo('chart');
+
+        $ob->title->text($translator->trans('Sales per day'));
+        $ob->subtitle->text($tag->getName());
+
+        $ob->xAxis->categories(array_keys($sales));
+        $ob->xAxis->title(array('text' => $translator->trans('Day')));
+        $ob->yAxis->title(array('text' => $translator->trans('Euro')));
+
+        $ob->series($series);
+
+        return $this->render(
+            'AffiliateDashboardBundle:Chart:chart.html.twig',
+            array('chart' => $ob)
+        );
+    }
+
+    /**
+     * Sales per month overview
+     *
+     * @Route("/tags", name="chart_tags")
+     * @Method("GET")
+     */
+    public function tagsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $translator = $this->get('translator');
+
+        $tags = $em->getRepository('AffiliateDashboardBundle:Tag')->findAll();
+
+        // Chart
+        $series = array();
+        $keys = array();
+
+        /** @var $tag Tag */
+        foreach ($tags as $tag) {
+            $sales = $em->getRepository('AffiliateDashboardBundle:Sale')->getSalesPerDay($tag);
+            $series[] = array(
+                'name' => $tag->getName(),
+                'data' => $sales
+            );
+            $keys = array_unique(array_merge($keys, array_keys($sales)));
+        }
+
+        sort($keys);
+
+        // Fill missing values with 0
+        foreach ($series as $key => $serie) {
+            $series[$key]['data'] = array_values(
+                array_replace(array_fill_keys($keys, array(0)), $serie['data'])
+            );
+        }
+
+        $ob = new Highchart();
+
+        $ob->chart->type('line');
+        $ob->chart->renderTo('chart');
+
+        $ob->title->text($translator->trans('Sales per day'));
+
+        $ob->xAxis->categories($keys);
+        $ob->xAxis->title(array('text' => $translator->trans('Day')));
+        $ob->xAxis->type('date');
+        $ob->yAxis->title(array('text' => $translator->trans('Euro')));
+
+        $ob->series($series);
+
+        return $this->render(
+            'AffiliateDashboardBundle:Chart:chart.html.twig',
+            array('chart' => $ob)
+        );
+    }
 }
--- src/AffiliateDashboardBundle/Repository/SaleRepository.php
+++ src/AffiliateDashboardBundle/Repository/SaleRepository.php
@@ -2,6 +2,8 @@
 
 namespace AffiliateDashboardBundle\Repository;
 
+use AffiliateDashboardBundle\Entity\Tag;
+
 /**
  * SaleRepository
  *
@@ -19,11 +21,30 @@ class SaleRepository extends \Doctrine\ORM\EntityRepository
         $subselect = $this->createQueryBuilder('s')
             ->select('YEAR(s.date) AS agYear, MONTH(s.date) AS agMonth, SUM(s.earnings) AS agEuro')
             ->groupBy('agYear')
-            ->addGroupBy('agMonth');
+            ->addGroupBy('agYear, agMonth');
 
         $arr = array();
         foreach ($subselect->getQuery()->getArrayResult() as $item) {
             $key = sprintf('%d-%02d', $item['agYear'], $item['agMonth']);
+            $arr[$key] = array(
+                (float)$item['agEuro']
+            );
+        }
+        return $arr;
+    }
+
+    public function getSalesPerDay(Tag $tag)
+    {
+        $subselect = $this->createQueryBuilder('s')
+            ->select('YEAR(s.date) AS agYear, MONTH(s.date) AS agMonth, DAY(s.date) AS agDay, SUM(s.earnings) AS agEuro')
+            ->where('s.affiliateTag = :tag')
+            ->groupBy('agYear')
+            ->addGroupBy('agYear, agMonth, agDay')
+            ->setParameter('tag', $tag);
+
+        $arr = array();
+        foreach ($subselect->getQuery()->getArrayResult() as $item) {
+            $key = sprintf('%d-%02d-%02d', $item['agYear'], $item['agMonth'], $item['agDay']);
             $arr[$key] = array(
                 (float)$item['agEuro']
             );
